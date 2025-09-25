9d2cc1e83b6a9b8ba52fad29e82343107b939e57
Lillie <chapman.lillie@gmail.com>
2025-09-25 10:09:13 +1000

    init: Models, controllers and schema folders & files created. Main.py defined to import all controllers and register all blueprints.

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask
-from init import db
 import os
+
+from flask import Flask
+
+from init import db
+from controllers.cli_controller import db_commands
+from controllers.user_controller import users_bp
+from controllers.organiser_controller import organisers_bp
+from controllers.event_controller import events_bp
+from controllers.booking_controller import bookings_bp
+
 
 def create_app():
     app = Flask(__name__)
     print("Flask server started.")
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI")
+    app.json.sort_keys = False
     db.init_app(app)
+    app.register_blueprint(db_commands)
+    app.register_blueprint(users_bp)
+    app.register_blueprint(organisers_bp)
+    app.register_blueprint(events_bp)
+    app.register_blueprint(bookings_bp)
+
     return app
