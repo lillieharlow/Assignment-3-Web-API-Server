70d3cb4710f845778d0d051622c0306944d84e79
Lillie <chapman.lillie@gmail.com>
2025-09-30 13:31:12 +1000

    feat(organiser): Defined PATCH/PUT routes.

--- controllers/organiser_controller.py
+++ controllers/organiser_controller.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError, DataError
 from psycopg2 import errors
+from marshmallow import ValidationError
 
 from init import db
 from models.organiser import Organiser
@@ -42,3 +43,25 @@ def create_a_organiser():
     db.session.commit()
     return organiser_schema.dump(new_organiser), 201
 
+# PATCH/PUT /id (update organiser by id)
+@organisers_bp.route("/<int:organiser_id>", methods = ["PUT", "PATCH"])
+def update_a_organiser(organiser_id):
+    stmt = db.select(Organiser).where(Organiser.organiser_id == organiser_id)
+    organiser = db.session.scalar(stmt)
+    if not organiser:
+        return {"message": f"Organiser with {organiser_id} id doesn't exist."}, 404
+    try:
+        update_organiser = organiser_schema.load(
+            request.get_json(),
+            instance = organiser,
+            session = db.session,
+            partial = True
+        )
+        db.session.commit()
+        return organiser_schema.dump(update_organiser), 200
+    except ValidationError as e:
+        return e.messages, 400
+    except IntegrityError as e:
+        return {"message": "Integrity error: likely a duplicate entry or invalid Foreign Key."}, 409
+    except DataError as e:
+        return {"message": "Data error: e.orig.diag.message_primary}."}, 400
